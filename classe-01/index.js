4b88abe81b847054f998687c1e139fb65439fc1e
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-12-23 16:17:20 -0300

    Classe 01 - Finalizado

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,1 +1,8 @@
 const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+
+
+//Filter retorna um novo array
+
+const cidadesModificado = cidades.filter(x => x.length <= 8);
+
+console.log(cidadesModificado);
