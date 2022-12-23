eb1e45c3d2bc6aff183fd2fc4a28745fe3b8537d
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-12-23 17:56:08 -0300

    Classe 02 - Finalizado

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,6 +1,9 @@
-const endereços = [
+const enderecos = [
     { cep: 00111222, rua: "Rua dos Artistas" },
     { cep: 00111333, rua: "Rua Augusta" },
     { cep: 00222444, rua: "Avenida Paralela" },
     { cep: 11222333, rua: "Rua Carlos Gomes" },
 ];
+
+const endecoLocalizado = enderecos.find(x => x.cep === 00222444);
+console.log(endecoLocalizado.rua);
