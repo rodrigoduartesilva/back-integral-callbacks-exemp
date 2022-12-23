884dae11ace5689b4c88f2df0fc076a7466cd497
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-12-23 18:43:53 -0300

    Classe 03 - Finalizado

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,6 @@
 const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
+
+const primeiraLetraMaiuscula = frutas.map((x, i) => `${i} - ${x.slice(0, 1).toUpperCase()
+    + x.slice(1).toLowerCase()}`);
+
+console.log(primeiraLetraMaiuscula);
