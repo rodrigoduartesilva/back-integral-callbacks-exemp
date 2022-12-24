c390763e06a1e24c488dd971b857a194d89bba4d
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-12-24 17:17:25 -0300

    Extra 08 - Finalizado

--- extra-08/index.js
+++ extra-08/index.js
@@ -1,1 +1,7 @@
-const nomes = ['Maria', 'João', 'José', 'Antonio', 'Beatriz', 'Camila'];
+const nomes = ['Maria', 'João', 'José', 'Antonio', 'Beatriz', 'Camila', 'amanda'];
+
+const nomesFiltrados = nomes.filter((x) => {
+    return x.substr(0, 1).toLowerCase() == 'a';
+});
+
+console.log(nomesFiltrados);
