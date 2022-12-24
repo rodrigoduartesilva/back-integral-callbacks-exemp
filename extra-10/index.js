687354cd488ae072eddf7a81d2698b9b7e539ff1
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-12-24 17:33:19 -0300

    Extra 10 - Finalizado

--- extra-10/index.js
+++ extra-10/index.js
@@ -1,1 +1,15 @@
-const notas = [6, 8, 9, 8];
+const notas = [6, 20, 33, 454, 8, 9, 9, 6, 8, 9, 20, 20, 33]
+
+const arrayNumeroUnico = [];
+
+notas.forEach(function (item) {
+    const verificaExistencia = arrayNumeroUnico.find(function (valor) {
+        return valor == item;
+    });
+
+    if (!verificaExistencia) {
+        arrayNumeroUnico.push(item);
+    }
+});
+
+console.log(arrayNumeroUnico);
