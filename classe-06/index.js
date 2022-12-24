b60e71d83b6e428d3f212e526d7c22b259f1756e
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2022-12-23 23:03:24 -0300

    Classe 06 - Finalizado

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,15 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+let i = 0;
+
+const retornoJogadores = function imprimirJogadores() {
+    if (i === jogadores.length) {
+        console.log('A rodada terminou!');
+        clearInterval(intervalo);
+    } else {
+        console.log(jogadores[i]);
+        i++;
+    }
+}
+
+
+const intervalo = setInterval(retornoJogadores, 2000);
